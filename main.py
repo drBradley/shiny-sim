9fbcae2eed6a5268329b76027176aa347565bd1d
Bartosz Boguniewicz <b.boguniewicz@gmail.com>
2014-04-03 00:42:38 +0200

    Use data from files to init and configure system

--- main.py
+++ main.py
@@ -59,33 +59,33 @@ def run():
     integrator.repaint()
     timer.start(delay)
 
+
 if __name__ == '__main__':
 
-    dt = 0.01
-    k = 1
+    if len(sys.argv) == 3:
 
-    bodies = [Body(Vector(100, 100), 1000),
-              Body(Vector(200, 100), 1),
-              Body(Vector(300, 100), 1000)]
+        dt = 0.01
+        k = 1
 
-    interactions = [String(bodies[0],
-                           bodies[1],
-                           k),
-                    String(bodies[1],
-                           bodies[2],
-                           k),
-                ]
+        bodies = []
+        interactions = []
 
-    app = QtGui.QApplication(sys.argv)
-    integrator = Integrator()
+        app = QtGui.QApplication(sys.argv)
+        integrator = Integrator()
 
-    integrator.add_bodies(bodies)
-    integrator.add_interactions(interactions)
+        init_system(sys.argv[1], bodies, interactions)
 
-    integrator.shift_body(1, Vector(1, 0, 0))
+        integrator.add_bodies(bodies)
+        integrator.add_interactions(interactions)
 
-    timer = QTimer()
-    timer.timeout.connect(run)
-    timer.start(50)
+        configure_system(sys.argv[2], integrator)
 
-    sys.exit(app.exec_())
+        timer = QTimer()
+        timer.timeout.connect(run)
+        timer.start(50)
+
+        sys.exit(app.exec_())
+
+    else:
+
+        print "Insufficient amount of parameters"
