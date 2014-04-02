540dc244203c2876baa8dc2b323ea4e0f3cb847b
Bartosz Boguniewicz <b.boguniewicz@gmail.com>
2014-04-02 23:08:22 +0200

    Clean up after old version

--- body.py
+++ body.py
@@ -21,14 +21,14 @@ class Body(object):
 
     def calculate(self, dt):
         # calculate new position using Verlet algorith
-        self.acceleration = self.compute_acceleration()
-        self.new_position = 2 * self.position - self.old_position - self.acceleration * dt**2
+        self.force = self.compute_force()
+        self.new_position = 2 * self.position - self.old_position - self.force * dt**2
 
         self.old_position = self.position;
         self.position = self.new_position;
         self.acting_forces = []
 
-    def compute_acceleration(self):
+    def compute_force(self):
 
         acc = Vector(0, 0, 0)
         for force in self.acting_forces:
