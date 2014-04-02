2c0341a250fdd3de85d24c74602fb062a89382a7
Bartosz Boguniewicz <b.boguniewicz@gmail.com>
2014-04-02 23:43:35 +0200

    Move Integrator to seperate file

---
+++ integrator.py
@@ -0,0 +1,84 @@
+import sys
+
+from PyQt4 import QtGui, QtCore
+from PyQt4.QtCore import QTimer
+
+from body import Body
+from string import String
+from cartesian import Vector
+
+delay = 10
+
+class Integrator(QtGui.QWidget):
+
+    def __init__(self):
+
+        super(Integrator, self).__init__()
+
+        self.show()
+
+        self.bodies = []
+        self.interactions = []
+
+    def add_bodies(self, bodies):
+
+        self.bodies = bodies
+
+    def add_body(self, body):
+
+        if isinstance(body, Body):
+
+            self.bodies.append(body)
+
+        else:
+
+            print "Not a physical body"
+
+    def add_interactions(self, interactions):
+
+        self.interactions = interactions
+
+    def shift_body(self, index_of_body, shift):
+
+        self.bodies[index_of_body].move_by(shift)
+
+    def integrate(self, dt):
+
+        for interaction in self.interactions:
+
+            interaction.compute()
+
+        for body in self.bodies:
+
+            body.calculate(dt)
+
+    def show_bodies(self):
+
+        for body in self.bodies:
+
+            print str(self.bodies.index(body)) + " " + body.show()
+
+    def show_interactions(self):
+
+        for interaction in self.interactions:
+
+            print interaction.show()
+
+    def paintEvent(self, event):
+
+        painter = QtGui.QPainter()
+        painter.begin(self)
+
+        for body in self.bodies:
+
+            body.draw(painter)
+
+        painter.end()
+
+    def keyPressEvent(self, event):
+
+        key = event.key()
+
+        if key == QtCore.Qt.Key_Escape:
+
+            self.close()
